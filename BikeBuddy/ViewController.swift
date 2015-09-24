881e24e6b813a5a604300b8d74a1ebb419e0b5df
Kyle McKee <mckee.kyle@gmail.com>
2015-09-23 22:48:55 -0400

    Added color and rssi output

--- BikeBuddy/ViewController.swift
+++ BikeBuddy/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")!, identifier: "RadBeacon");
+    
+    @IBOutlet weak var distanceLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.startRangingBeaconsInRegion(region)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        
+        var color: UIColor
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon            
+            switch closestBeacon.proximity.rawValue {
+            case 1:
+                color = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+            case 2:
+                color = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+            case 3:
+                color = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+            case 4:
+                color = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+            case 5:
+                color = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
+            default:
+                color = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+            }
+            distanceLabel.text = String(closestBeacon.rssi)
+        } else {
+            color = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+            distanceLabel.text = "N/A"
+        }
+        
+        self.view.backgroundColor = color
+    }
 
 }
 
